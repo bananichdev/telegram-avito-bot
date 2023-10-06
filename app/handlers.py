759f8dadc58f5d2e293f911b48554f477616d921
Панин Дмитрий Владимирович <dimitri-05@mail.ru>
2023-10-06 20:50:14 +0300

    Фикс багов

--- app/handlers.py
+++ app/handlers.py
@@ -6,8 +6,12 @@ from aiogram.fsm.state import State, StatesGroup
 from aiogram.filters import Filter, StateFilter
 from app.keyboards import main, to_main, off_notif
 from app.parser import AvitoParse
+import validators
+import logging
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 router = Router()
+stop_send_notifications = False
 stop_send_messages = False
 
 
@@ -23,6 +27,31 @@ class FSMNotification(StatesGroup):
 class PagesValidator(Filter):
     async def __call__(self, message: Message):
         return message.text.isdigit() and 1 <= int(message.text) <= 100
+
+
+async def send_results(cards_obj: AvitoParse, message: Message):
+    global stop_send_messages
+    stop_send_messages = False
+    cards = await asyncio.to_thread(cards_obj.parse)
+    for card in cards:
+        if stop_send_messages:
+            break
+        await message.answer(
+            f'Название: {card["name"]}\n'
+            f'Цена: {card["price"]}\n'
+            f'Описание: {card["description"]}\n'
+            f'URL: {card["url"]}'
+        )
+        await message.answer(
+            text='Если хотите остановить отправку, то нажмите кнопку',
+            reply_markup=to_main
+        )
+        await asyncio.sleep(5)
+    else:
+        await message.answer(
+            text='Можете дальше пользоваться ботом',
+            reply_markup=main
+        )
 
 
 @router.message(F.text == '/start')
@@ -42,13 +71,22 @@ async def start(message: Message, state: FSMContext):
 async def gns_callback(callback: CallbackQuery, state: FSMContext):
     await state.set_state(FSMSearch.pages)
     await state.update_data(command=callback.data.split('_')[0])
-    await callback.message.delete()
-    await callback.message.answer(
-        text='Введите количество страниц, на которых будут искаться объявления\n'
-             '<b>Число от 1 до 100</b>',
-        parse_mode='html',
-        reply_markup=to_main
-    )
+    try:
+        await callback.message.delete()
+        await callback.message.answer(
+            text='Введите количество страниц, на которых будут искаться объявления\n'
+                 '<b>Число от 1 до 100</b>',
+            parse_mode='html',
+            reply_markup=to_main
+        )
+    except Exception as e:
+        logging.error(f'Error: {e}', exc_info=True)
+        await callback.message.edit_text(
+            text='Введите количество страниц, на которых будут искаться объявления\n'
+                 '<b>Число от 1 до 100</b>',
+            parse_mode='html',
+            reply_markup=to_main
+        )
 
 
 '''****************************start****************************'''
@@ -77,19 +115,8 @@ async def pages(message: Message, state: FSMContext):
             text='<b>Ничего не пишите до окончания работы бота!</b>',
             parse_mode='html'
         )
-        cards = await asyncio.to_thread(AvitoParse(url='https://clck.ru/35o5vb', count=count_pages).parse)
-        for card in cards:
-            await message.answer(
-                f'Название: {card["name"]}\n'
-                f'Цена: {card["price"]}\n'
-                f'Описание: {card["description"]}\n'
-                f'URL: {card["url"]}'
-            )
-            await asyncio.sleep(2)
-        await message.answer(
-            text='Можете дальше пользоваться ботом',
-            reply_markup=main
-        )
+        cards_obj = AvitoParse(url='https://www.avito.ru/all/odezhda_obuv_aksessuary/obuv_muzhskaya-ASgBAgICAUTeArqp1gI?cd=1&f=ASgBAQECAUTeArqp1gICQOK8DTS8q9YC8NE07tE0uIcO1ObykwHk8pMB4vKTAeDykwHe8pMB3PKTAdrykwHY8pMB1vKTAdTykwHS8pMB0PKTAc7ykwEBRcaaDBd7ImZyb20iOjUwMCwidG8iOjE1MDAwfQ&q=%D0%BA%D1%80%D0%BE%D1%81%D1%81%D0%BE%D0%B2%D0%BA%D0%B8+nike+%D0%BE%D1%80%D0%B8%D0%B3%D0%B8%D0%BD%D0%B0%D0%BB&s=104', count=count_pages)
+        await send_results(cards_obj, message)
 
 
 '''****************************pages****************************'''
@@ -97,7 +124,7 @@ async def pages(message: Message, state: FSMContext):
 '''*****************************url*****************************'''
 
 
-@router.message(FSMSearch.url, F.text.func(lambda text: 'avito.ru/' in text))
+@router.message(FSMSearch.url, F.text.func(lambda text: 'avito.ru/' in text and validators.url(text)))
 async def url(message: Message, state: FSMContext):
     data = await state.get_data()
     await state.clear()
@@ -109,19 +136,8 @@ async def url(message: Message, state: FSMContext):
         text='<b>Ничего не пишите до окончания работы бота!</b>',
         parse_mode='html'
     )
-    cards = await asyncio.to_thread(AvitoParse(url=message.text, count=data['count_pages']).parse)
-    for card in cards:
-        await message.answer(
-            f'Название: {card["name"]}\n'
-            f'Цена: {card["price"]}\n'
-            f'Описание: {card["description"]}\n'
-            f'URL: {card["url"]}'
-        )
-        await asyncio.sleep(2)
-    await message.answer(
-        text='Можете дальше пользоваться ботом',
-        reply_markup=main
-    )
+    cards_obj = AvitoParse(url=message.text, count=data['count_pages'])
+    await send_results(cards_obj, message)
 
 
 '''*****************************url*****************************'''
@@ -131,15 +147,24 @@ async def url(message: Message, state: FSMContext):
 
 @router.callback_query(F.data == 'notif_btn')
 async def notif_callback(callback: CallbackQuery, state: FSMContext):
-    global stop_send_messages
-    stop_send_messages = False
-    await callback.message.delete()
-    await state.set_state(FSMNotification.notification)
-    await callback.message.answer(
-        text='Введите URL Авито страницы\n'
-             'Если хотите получать уведомления о новых объявлениях кроссовок Nike, то введите 0',
-        reply_markup=to_main
-    )
+    global stop_send_notifications
+    stop_send_notifications = False
+    try:
+        await callback.message.delete()
+        await callback.message.answer(
+            text='Введите URL Авито страницы\n'
+                 'Если хотите получать уведомления о новых объявлениях кроссовок Nike, то введите 0',
+            reply_markup=to_main
+        )
+    except Exception as e:
+        logging.error(f'Error: {e}', exc_info=True)
+        await callback.message.edit_text(
+            text='Введите URL Авито страницы\n'
+                 'Если хотите получать уведомления о новых объявлениях кроссовок Nike, то введите 0',
+            reply_markup=to_main
+        )
+    finally:
+        await state.set_state(FSMNotification.notification)
 
 
 @router.message(FSMNotification.notification, F.text.func(lambda text: 'avito.ru/' in text or text == '0'))
@@ -150,14 +175,14 @@ async def notifications(message: Message, state: FSMContext):
     )
     await state.clear()
     if message.text == '0':
-        notif_obj = AvitoParse(url='https://clck.ru/35o5vb')
+        notif_obj = AvitoParse(url='https://www.avito.ru/all/odezhda_obuv_aksessuary/obuv_muzhskaya-ASgBAgICAUTeArqp1gI?cd=1&f=ASgBAQECAUTeArqp1gICQOK8DTS8q9YC8NE07tE0uIcO1ObykwHk8pMB4vKTAeDykwHe8pMB3PKTAdrykwHY8pMB1vKTAdTykwHS8pMB0PKTAc7ykwEBRcaaDBd7ImZyb20iOjUwMCwidG8iOjE1MDAwfQ&q=%D0%BA%D1%80%D0%BE%D1%81%D1%81%D0%BE%D0%B2%D0%BA%D0%B8+nike+%D0%BE%D1%80%D0%B8%D0%B3%D0%B8%D0%BD%D0%B0%D0%BB&s=104')
     else:
         notif_obj = AvitoParse(url=message.text)
     await asyncio.to_thread(notif_obj.set_up)
     count_notif = 0
-    global stop_send_messages
+    global stop_send_notifications
     while True:
-        if stop_send_messages:
+        if stop_send_notifications:
             await asyncio.to_thread(notif_obj.quit)
             break
         if count_notif == 0:
@@ -221,19 +246,34 @@ async def invalid_pages(message: Message):
 
 @router.callback_query(F.data == 'inf_btn')
 async def inf_callback(callback: CallbackQuery):
-    await callback.message.delete()
-    await callback.message.answer(
-        text='Алгоритм данного бота рассчитан на поиск <em>оригинальных вещей</em>\n'
-        'Однако, пока что, бот может находить сомнительные предложения(даркреселлеров)\n'
-        'Но все-же большинство объявлений - оригинальные вещи\n\n\n'
-        'Описание возможностей:\n\n'
-        'Поиск кроссовок Nike - ищет объявления с кроссовками Nike\n\n'
-        'Поиск по URL - ищет объявления по заданному URL\n\n'
-        'Режим уведомлений - ищет самые новые объявления по заданному URL\n\n\n'
-        'По вопросам сотрудничества и улучшения функционала писать сюда: @vzlomjopii',
-        parse_mode='html',
-        reply_markup=to_main
-    )
+    try:
+        await callback.message.delete()
+        await callback.message.answer(
+            text='Алгоритм данного бота рассчитан на поиск <em>оригинальных вещей</em>\n'
+                 'Однако, пока что, бот может находить сомнительные предложения(даркреселлеров)\n'
+                 'Но все-же большинство объявлений - оригинальные вещи\n\n\n'
+                 'Описание возможностей:\n\n'
+                 'Поиск кроссовок Nike - ищет объявления с кроссовками Nike\n\n'
+                 'Поиск по URL - ищет объявления по заданному URL\n\n'
+                 'Режим уведомлений - ищет самые новые объявления по заданному URL\n\n\n'
+                 'По вопросам сотрудничества и улучшения функционала писать сюда: @vzlomjopii',
+            parse_mode='html',
+            reply_markup=to_main
+        )
+    except Exception as e:
+        logging.error(f'Error: {e}', exc_info=True)
+        await callback.message.edit_text(
+            text='Алгоритм данного бота рассчитан на поиск <em>оригинальных вещей</em>\n'
+                 'Однако, пока что, бот может находить сомнительные предложения(даркреселлеров)\n'
+                 'Но все-же большинство объявлений - оригинальные вещи\n\n\n'
+                 'Описание возможностей:\n\n'
+                 'Поиск кроссовок Nike - ищет объявления с кроссовками Nike\n\n'
+                 'Поиск по URL - ищет объявления по заданному URL\n\n'
+                 'Режим уведомлений - ищет самые новые объявления по заданному URL\n\n\n'
+                 'По вопросам сотрудничества и улучшения функционала писать сюда: @vzlomjopii',
+            parse_mode='html',
+            reply_markup=to_main
+        )
 
 
 '''*****************************inf*****************************'''
@@ -244,14 +284,26 @@ async def inf_callback(callback: CallbackQuery):
 @router.callback_query(F.data.in_({'to_main_btn', 'off_btn'}))
 async def to_main_callback(callback: CallbackQuery, state: FSMContext):
     global stop_send_messages
-    stop_send_messages = True
+    global stop_send_notifications
+    if callback.data == 'off_btn':
+        stop_send_notifications = True
+    else:
+        stop_send_messages = True
     await state.clear()
-    await callback.message.delete()
-    await callback.message.answer(
-        text='Помогу найти <b><em>оригинальные</em></b> вещи на Авито',
-        parse_mode='html',
-        reply_markup=main
-    )
+    try:
+        await callback.message.delete()
+        await callback.message.answer(
+            text='Помогу найти <b><em>оригинальные</em></b> вещи на Авито',
+            parse_mode='html',
+            reply_markup=main
+        )
+    except Exception as e:
+        logging.error(f'Error: {e}', exc_info=True)
+        await callback.message.edit_text(
+            text='Помогу найти <b><em>оригинальные</em></b> вещи на Авито',
+            parse_mode='html',
+            reply_markup=main
+        )
 
 
 '''***************************to_main***************************'''
--- app/parser/parseroop.py
+++ app/parser/parseroop.py
@@ -1,12 +1,18 @@
 import undetected_chromedriver as uc
 import os
+import logging
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from fake_useragent import UserAgent
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("selenium_parser")
+
 
 class AvitoParse:
-    def __init__(self, version_main: int = 117, url: str = '', items: list = ['оригинал'], count: int = 10):
+    def __init__(self, version_main: int = 117, url: str = '', items=None, count: int = 10):
+        if items is None:
+            items = ['ориг']
         self.version_main = version_main
         self.url = url
         self.items = items
@@ -14,10 +20,11 @@ class AvitoParse:
         self.data = []
 
     def __set_up(self):
-        options = Options()
-        options.add_argument(f'user-agent={UserAgent(browsers=["chrome"]).random}')
-        options.add_argument('--headless')
         system = os.getenv('SYSTEM')
+        options = Options()
+        user_agent = UserAgent(browsers=['chrome'], os=system).random
+        options.add_argument(f'user-agent={user_agent}')
+        options.add_argument('--headless')
         if system == 'linux':
             options.binary_location = 'app/parser/chrome-linux64/chrome'
         else:
@@ -42,26 +49,30 @@ class AvitoParse:
             self.driver.find_element(By.CSS_SELECTOR, value='[data-marker="pagination-button/nextPage"]').click()
 
     def __parse_page(self):
-        cards = self.driver.find_elements(By.CSS_SELECTOR, value='div[class*="item-content"]')
+        cards = self.driver.find_elements(By.CSS_SELECTOR, value='div[class*="iva-item-body"]')
         for card in cards:
-            name = card.find_element(By.CSS_SELECTOR, value='[itemprop="name"]').text
-            price = card.find_element(By.CSS_SELECTOR, value='[itemprop="price"]').get_attribute('content')
-            description = card.find_element(By.CSS_SELECTOR, value='[class*="iva-item-descriptionStep"]').text
-            url = card.find_element(By.CSS_SELECTOR, value='[data-marker="item-title"]').get_attribute('href')
-            if any(word.lower() in description.lower() for word in self.items) and len(description) <= 250 and all(word not in description.lower() for word in ('качеств', 'люкс', 'материал')) and all(word not in name.lower() for word in ('качеств', 'люкс', 'материал')):
-                data = {
-                    'name': name,
-                    'price': price,
-                    'description': description,
-                    'url': url
-                }
-                self.data.append(data)
+            try:
+                name = card.find_element(By.CSS_SELECTOR, value='h3[itemprop="name"]').text
+                price = card.find_element(By.CSS_SELECTOR, value='meta[itemprop="price"]').get_attribute('content')
+                description = card.find_element(By.CSS_SELECTOR, value='div[class*="iva-item-description"]').text
+                url = card.find_element(By.CSS_SELECTOR, value='a[data-marker="item-title"]').get_attribute('href')
+                if any(word.lower() in description.lower() for word in self.items) and len(description) <= 250 and all(word not in description.lower() for word in ('качеств', 'люкс', 'материал', 'не ориг')) and all(word not in name.lower() for word in ('качеств', 'люкс', 'материал')):
+                    data = {
+                        'name': name,
+                        'price': price,
+                        'description': description,
+                        'url': url
+                    }
+                    self.data.append(data)
+            except Exception as e:
+                logging.error(f'Error: {e.args}', exc_info=True)
+                break
 
     def __parse_first_element(self):
         name = self.driver.find_element(By.XPATH, value='//h3[@itemprop="name"]').text
-        price = self.driver.find_element(By.CSS_SELECTOR, value='[itemprop="price"]').get_attribute('content')
-        description = self.driver.find_element(By.CSS_SELECTOR, value='[class*="iva-item-descriptionStep"]').text
-        url = self.driver.find_element(By.CSS_SELECTOR, value='[data-marker="item-title"]').get_attribute('href')
+        price = self.driver.find_element(By.CSS_SELECTOR, value='meta[itemprop="price"]').get_attribute('content')
+        description = self.driver.find_element(By.CSS_SELECTOR, value='div[class*="iva-item-description"]').text
+        url = self.driver.find_element(By.CSS_SELECTOR, value='a[data-marker="item-title"]').get_attribute('href')
         if all(word not in description.lower() for word in ('качеств', 'люкс', 'материал')) and all(word not in name.lower() for word in ('качеств', 'люкс', 'материал')):
             self.data = {
                 'name': name,
